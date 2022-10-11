2507ca322a63c34060be236e2aa1f56b50e5d994
Michał Łępicki <michallepicki@users.noreply.github.com>
2022-10-11 09:07:25 +0200

    fix run script, tweak styling

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # rm Gemfile
 # rm Gemfile.lock
-# echo "source \"https://rubygems.org\"\ngem 'github-pages'" > Gemfile
+# echo -e "source \"https://rubygems.org\"\ngem 'github-pages'" > Gemfile
 # bundle install
 bundle exec jekyll serve --livereload --drafts --future --port 4001 --host 0.0.0.0
